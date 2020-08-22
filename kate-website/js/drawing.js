70f939b12be9d9367baf41cab6cfaa9c641000f2
Wei Zhou <wei@Weis-iMac.local>
2020-08-22 11:26:00 -0400

    add images

--- kate-website/js/drawing.js
+++ kate-website/js/drawing.js
@@ -13,30 +13,31 @@
 //   "./imgs/kate-pic12.jpg"
 // ];
 
-const imgUrls = [
-  "https://app.nihaocloud.com/f/91097548bb57484cab9a/?dl=1",
-  "https://app.nihaocloud.com/f/974e60d3cd39445983e2/?dl=1",
-  "https://app.nihaocloud.com/f/50639edc0b4842c78219/?dl=1",
-  "https://app.nihaocloud.com/f/a7a09d4f1b1845d9bf1d/?dl=1",
-  "https://app.nihaocloud.com/f/24f7bf9eb71e4c8daa1e/?dl=1",
-  "https://app.nihaocloud.com/f/5413ecd501ff4f21a9c7/?dl=1",
-  "https://app.nihaocloud.com/f/1f966f410c01468ba9e3/?dl=1",
-  "https://app.nihaocloud.com/f/28ec8b3438a943729e6a/?dl=1",
-  "https://app.nihaocloud.com/f/70aa439470a7488f8d08/?dl=1",
-  "https://app.nihaocloud.com/f/422abff15a224c679bb4/?dl=1",
-  "https://app.nihaocloud.com/f/4d897b604cd542beb608/?dl=1",
-  "./imgs/kate-pic12.jpg",
-  "https://app.nihaocloud.com/f/62ddddc574af4ca0aca8/?dl=1",
-  "https://app.nihaocloud.com/f/c83046ef59a74057bc05/?dl=1",
-  "https://app.nihaocloud.com/f/47a5c714c94c4bfc819f/?dl=1"
-];
+// const imgUrls = [
+//   "https://app.nihaocloud.com/f/91097548bb57484cab9a/?dl=1",
+//   "https://app.nihaocloud.com/f/974e60d3cd39445983e2/?dl=1",
+//   "https://app.nihaocloud.com/f/50639edc0b4842c78219/?dl=1",
+//   "https://app.nihaocloud.com/f/a7a09d4f1b1845d9bf1d/?dl=1",
+//   "https://app.nihaocloud.com/f/24f7bf9eb71e4c8daa1e/?dl=1",
+//   "https://app.nihaocloud.com/f/5413ecd501ff4f21a9c7/?dl=1",
+//   "https://app.nihaocloud.com/f/1f966f410c01468ba9e3/?dl=1",
+//   "https://app.nihaocloud.com/f/28ec8b3438a943729e6a/?dl=1",
+//   "https://app.nihaocloud.com/f/70aa439470a7488f8d08/?dl=1",
+//   "https://app.nihaocloud.com/f/422abff15a224c679bb4/?dl=1",
+//   "https://app.nihaocloud.com/f/4d897b604cd542beb608/?dl=1",
+//   "./imgs/kate-pic12.jpg",
+//   "https://app.nihaocloud.com/f/62ddddc574af4ca0aca8/?dl=1",
+//   "https://app.nihaocloud.com/f/c83046ef59a74057bc05/?dl=1",
+//   "https://app.nihaocloud.com/f/47a5c714c94c4bfc819f/?dl=1"
+// ];
+
+
+const imgIDs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
 var imageGallery = document.getElementById("myGallery");
 
-imgUrls.forEach((url) => {
+imgIDs.forEach((ID) => {
   let imgDiv =
-    '<div class="mb-3 pics animation"><img class="img-fluid" src=' +
-    url +
-    "></img></div>";
+    '<div class="mb-3 pics animation"><img class="img-fluid" src="./imgs/kate-pic' + id + '.jpg"></img></div>';
   imageGallery.innerHTML += imgDiv;
 });

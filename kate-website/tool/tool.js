e1b2bb40026b141960a13bdf136d6502209c12a9
Wei Zhou <wzhou8@gmail.com>
2020-09-04 21:25:42 -0400

    final touch on resize tool

--- kate-website/tool/tool.js
+++ kate-website/tool/tool.js
@@ -1,32 +1,43 @@
-const uploadInput = document.getElementById("upload-input")
+const uploadBtn = document.getElementById("upload-btn")
 const imagePreview = document.getElementById('image-preview');
+const resizeBtn = document.getElementById("resize-btn");
+const canvasDiv = document.getElementById("canvas-div");
 const canvas = document.getElementById("canvas-image");
 
-uploadInput.addEventListener("input", e => {
+imagePreview.style.display = "none";
+resizeBtn.style.display = "none";
+canvasDiv.style.display = "none";
+
+uploadBtn.addEventListener("input", e => {
     var reader = new FileReader();
     reader.onload = function(){
-        imagePreview.src = reader.result;
-        document.getElementById("btn-col").style.display = "block";
-    }
-    reader.readAsDataURL(e.target.files[0]);
+        uploadBtn.style.lineHeight = "40px";
 
+        imagePreview.src = reader.result;
+        imagePreview.style.width = "40%";
+        imagePreview.style.display = "block";
+
+        resizeBtn.style.display = "flex";
+        resizeBtn.style.justifyContent = "center";
+    }
+    reader.readAsDataURL(e.target.files[0]);    
 })
 
 const convertBtn = document.getElementById("convert-btn");
 
 convertBtn.addEventListener("click", e=>{
     canvas.width = canvas.height = 0;
-        let ctx = canvas.getContext("2d");
-        let imgwidth = imagePreview.naturalWidth;
-        let imgheight = imagePreview.naturalHeight;
-        let resizeWidth = document.getElementById("resize-width").value;
-        if(!resizeWidth) resizeWidth=512;
-        let resizeHeight = imgheight * resizeWidth / imgwidth;
-        canvas.width = resizeWidth;
-        canvas.height = resizeHeight;
-        ctx.drawImage(imagePreview, 0, 0, imgwidth, imgheight, 0, 0, resizeWidth, resizeHeight);
-        document.getElementById("canvas-col").style.display = "block";
-    }, false);
+    let ctx = canvas.getContext("2d");
+    let imgwidth = imagePreview.naturalWidth;
+    let imgheight = imagePreview.naturalHeight;
+    let resizeWidth = document.getElementById("resize-width").value;
+    if(!resizeWidth) resizeWidth=512;
+    let resizeHeight = imgheight * resizeWidth / imgwidth;
+    canvas.width = resizeWidth;
+    canvas.height = resizeHeight;
+    ctx.drawImage(imagePreview, 0, 0, imgwidth, imgheight, 0, 0, resizeWidth, resizeHeight);
+    canvasDiv.style.display = "block";
+}, false);
 
 
 function download(filename, link) { 

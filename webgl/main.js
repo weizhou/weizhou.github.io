95bde12a5c33ab2b6e2e7d59a8609336079d7f82
Wei Zhou <wzhou8@gmail.com>
2020-09-27 09:01:31 -0400

    fixed the resolution issue of canvas

--- webgl/main.js
+++ webgl/main.js
@@ -1,6 +1,9 @@
 const {mat4, vec3} = glMatrix;
 
 const canvas = document.querySelector('canvas');
+const canvasContainer = document.querySelector('div');
+canvas.width = canvasContainer.clientWidth;
+canvas.height = canvasContainer.clientHeight;
 const gl = canvas.getContext('webgl');
 
 if(!gl) {
@@ -14,12 +17,13 @@ function spherePointCloud (pointCount) {
     const r = () => Math.random() - 0.5;
     const inputPoint = [r(), r(), r()];
     const outputPoint = vec3.normalize(vec3.create(), inputPoint);
+    // const outputPoint = inputPoint;
     points.push(...outputPoint);
   }
 
   return points;
 }
-const vertexData = spherePointCloud(3e3);
+const vertexData = spherePointCloud(1e5);
 
 const positionBuffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
@@ -27,7 +31,7 @@ gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.STATIC_DRAW);
 
 const vertexShader = gl.createShader(gl.VERTEX_SHADER);
 gl.shaderSource(vertexShader, `
-precision mediump float;
+precision highp float;
 
 attribute vec3 position;
 varying vec3 vColor;
@@ -35,7 +39,8 @@ varying vec3 vColor;
 uniform mat4 matrix;
 
 void main() {
-  vColor = vec3(position.xy, 1);
+  // vColor = vec3(position.xy, 1);
+  vColor = vec3(0, 0.8, 1);
   gl_Position = matrix * vec4(position, 1);
   gl_PointSize = 0.01;
 }
@@ -44,7 +49,7 @@ gl.compileShader(vertexShader);
 
 const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
 gl.shaderSource(fragmentShader, `
-precision mediump float;
+precision highp float;
 
 varying vec3 vColor;
 
@@ -86,13 +91,14 @@ const mvMatrix = mat4.create();
 const mvpMatrix = mat4.create();
 
 mat4.translate(modelMatrix, modelMatrix, [0, 0, 0]);
-mat4.translate(viewMatrix, viewMatrix, [0, 0.1, 2]);
+mat4.translate(viewMatrix, viewMatrix, [0, 0, 1.7]);
 mat4.invert(viewMatrix, viewMatrix);
 
 function animate() {
   requestAnimationFrame(animate);
 
   mat4.rotateY(modelMatrix, modelMatrix, 0.01);
+  mat4.rotateX(modelMatrix, modelMatrix, 0.01);
 
   mat4.multiply(mvMatrix, viewMatrix, modelMatrix);
   mat4.multiply(mvpMatrix, projectionMatrix, mvMatrix);

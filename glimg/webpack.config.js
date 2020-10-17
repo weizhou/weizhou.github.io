8a21ddc60fa7747882f8b2c577de2153972527c9
Wei Zhou <wzhou8@gmail.com>
2020-10-17 14:50:01 -0400

    add logic to copy bundle js to project src

--- glimg/webpack.config.js
+++ glimg/webpack.config.js
@@ -1,10 +1,18 @@
 const path = require('path');
-
+const CopyPlugin = require('copy-webpack-plugin');
+ 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'glimg.bundle.js',
     libraryTarget: 'umd',
-  }
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: './dist/glimg.bundle.js', to: '../../webgl/' },
+      ],
+    }),
+  ],
 };
